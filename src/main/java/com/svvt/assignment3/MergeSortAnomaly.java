e9b3f9a3d3bef3c15d026d342023c33a602deacf
himaASu <145945991+himaASu@users.noreply.github.com>
2023-10-04 21:43:04 -0700

    Update MergeSortAnomaly.java

--- src/main/java/com/svvt/assignment3/MergeSortAnomaly.java
+++ src/main/java/com/svvt/assignment3/MergeSortAnomaly.java
@@ -1,74 +1,66 @@
 package com.svvt.assignment3;
-
 public class MergeSortAnomaly {
 
-	public static void main(String[] args) {
-		int[] arr = { 12, 11, 13, 5, 6, 7 };
-		mergeSort(arr, 0, arr.length - 1);
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        int arrayLength = arr.length;
 
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-	}
+        // Data flow anomaly #1: Unused variable
+        int unusedVariable = 10;
 
-	public static void mergeSort(int[] arr, int l, int r) {
-		if (l < r) {
-			int m = l + (r - l) / 2;
+        mergeSort(arr, 0, arrayLength - 1);
 
-			// Data Flow Anomaly 1: Unused variable
-			int z = 10;
+        // Data flow anomaly #2: Unreachable code after return
+        return;
+        System.out.println("This code is unreachable");
+    }
 
-			mergeSort(arr, l, m);
-			mergeSort(arr, m + 1, r);
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
 
-			merge(arr, l, m, r);
-		}
-	}
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
 
-	public static void merge(int[] arr, int l, int m, int r) {
-		int n1 = m - l + 1;
-		int n2 = r - m;
+            merge(arr, l, m, r);
+        }
+    }
 
-		int[] L = new int[n1];
-		int[] R = new int[n2];
+    public static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-		for (int i = 0; i < n1; i++) {
-			L[i] = arr[l + i];
-		}
-		for (int j = 0; j < n2; j++) {
-			R[j] = arr[m + 1 + j];
-		}
+        int[] L = new int[n1];
+        int[] R = new int[n2];
 
-		int i = 0, j = 0;
-		int k = l;
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
-			} else {
-				arr[k] = R[j];
-				j++;
-			}
-			k++;
-		}
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
 
-		while (i < n1) {
-			arr[k] = L[i];
-			i++;
-			k++;
-		}
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
 
-		// Data Flow Anomaly 2: This loop is redundant
-		while (j < n2) {
-			arr[k] = R[j];
-			j++;
-			k++;
-		}
-
-		// Data Flow Anomaly 3: Unreachable code
-		if (false) {
-			System.out.println("This line will never be executed.");
-		}
-	}
+        // Data flow anomaly #3: Unused loop
+        while (j < n2) {
+            // Loop does nothing
+            j++;
+        }
+    }
 }
